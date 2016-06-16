b49273bfd710f26a990a6f9bf43acc9eaad8ff40
ddddan <d1mullin@gmail.com>
2016-06-15 21:53:35 -0400

    progress on tabulate.js

--- routes/tabulate.js
+++ routes/tabulate.js
@@ -1,26 +1,46 @@
+/* PACKAGES */
 var fs = require('fs'),
     mongodb = require('mongodb'),
     JSONStream = require('JSONStream');
 
-/**
-* readFile(fileName) - Read the file into memory
-*
-* @param string fileName: The file to read
-* @return object: A reduced data set
-*/
-function readFile(fileName) {
-    var result = {};
-    var readableStream = fs.createReadStream('')
+/* GLOBALS - CONSTANTS */
+var C = {}
+C.fileName = '\\data\\gCycle-data\\Takeout\\Location History\\test.json';
 
+
+/**
+ * initDB() - Initialize the database
+ *
+ * @return mongodb - The database pointer
+ */
+function initDB() {
+    return null;
+}
+
+/**
+ * readFile(fileName) - Read the file into memory
+ *
+ * @param string fileName: The file to read
+ * @param mongodb db: The database in which to store the results
+ * @return object: A reduced data set
+ */
+function readFile(fileName, db) {
+    var result = {};
+
+    fs.createReadStream(fileName)
+        .pipe(JSONStream.parse('locations'))
+        .on('data', function (data) {
+            console.dir(data);
+        });
 
     return result;
 }
 
 
-// 1. Read in the file
+// 1. Read in the file and store into DB
+var db = initDB();
+res = readFile(C.fileName, db);
 
-// 2. Store data in DB
-// 3. Do necessary filtering
-// 4. Organize by date (and start time)
-// 5. Output report
-
+// 2. Do necessary filtering
+// 3. Organize by date (and start time)
+// 4. Output report
