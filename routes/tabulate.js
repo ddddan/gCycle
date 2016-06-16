18a8f202004f263ba79634693e98c077de8808a8
ddddan <d1mullin@gmail.com>
2016-06-16 17:28:45 -0400

    tabulate.js - dev update

--- routes/tabulate.js
+++ routes/tabulate.js
@@ -1,36 +1,32 @@
 /* PACKAGES */
 var fs = require('fs'),
-    mongodb = require('mongodb'),
-    JSONStream = require('JSONStream');
+    MongoClient = require('mongodb').MongoClient,
+    JSONStream = require('JSONStream'),
+    test = require('assert');
 
 /* GLOBALS - CONSTANTS */
 var C = {}
 C.fileName = '\\data\\gCycle-data\\Takeout\\Location History\\test.json';
+C.mongoURL = 'mongodb://localhost:27017/gCycle';
 
-
-/**
- * initDB() - Initialize the database
- *
- * @return mongodb - The database pointer
- */
-function initDB() {
-    return null;
-}
 
 /**
  * readFile(fileName) - Read the file into memory
  *
  * @param string fileName: The file to read
- * @param mongodb db: The database in which to store the results
+ * @param MongoDB.Collection col: The database in which to store the results
  * @return object: A reduced data set
  */
-function readFile(fileName, db) {
+function readFile(fileName, col) {
     var result = {};
 
     fs.createReadStream(fileName)
         .pipe(JSONStream.parse('locations'))
         .on('data', function (data) {
-            console.dir(data);
+            col.insertMany(data, function(err, r){
+                test.equal(null, err);
+                test.equal(data.length, r.insertedCount);
+            });
         });
 
     return result;
@@ -38,8 +34,17 @@ function readFile(fileName, db) {
 
 
 // 1. Read in the file and store into DB
-var db = initDB();
-res = readFile(C.fileName, db);
+MongoClient.connect(C.mongoURL, function (err, db) {
+    test.equal(null, err);
+
+    // Add a collection to hold the raw data
+    db.createCollection('rawData', function(err, col){
+        test.equal(null, err);
+        // Read the data
+        readFile(C.fileName, col);
+    });
+
+});
 
 // 2. Do necessary filtering
 // 3. Organize by date (and start time)
