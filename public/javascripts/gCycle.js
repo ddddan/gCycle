e4109aacef0420d6677f499aa228278bb41b6276
ddddan <d1mullin@gmail.com>
2016-06-29 23:21:50 -0400

    Minor update - gCycle.js - display proportional intervals

--- public/javascripts/gCycle.js
+++ public/javascripts/gCycle.js
@@ -19,6 +19,7 @@ function addConfidenceGraph(eTD, confArray) {
     C.bottom = C.canvasHeight - C.marginY;
     C.leftBound = C.marginX;
     C.rightBound = C.canvasWidth - C.marginX;
+    C.innerWidth = C.rightBound - C.leftBound;
     C.colorMap = [
         '#DAFFFF',
         '#C1FEFF',
@@ -40,11 +41,13 @@ function addConfidenceGraph(eTD, confArray) {
 
     var ctx = cvs.getContext('2d');
 
-    ctx.strokeRect(C.leftBound, C.top, (C.rightBound - C.leftBound), (C.bottom - C.top));
+    ctx.strokeRect(C.leftBound, C.top, C.innerWidth, (C.bottom - C.top));
 
-    var sliceWidth = (C.rightBound - C.leftBound) / confArray.length;
-    for (var i = 0; i < confArray.length; i++) {
-        var leftBound = i * sliceWidth + C.leftBound;
+    //var sliceWidth = (C.rightBound - C.leftBound) / confArray.length;
+    for (var i = 0; i < confArray.length - 1; i++) {
+        // var leftBound = i * sliceWidth + C.leftBound;
+        var leftBound = ((confArray[i].timestampMs - confArray[0].timestampMs) / (confArray[confArray.length-1].timestampMs - confArray[0].timestampMs)) * C.innerWidth + C.leftBound;
+        var sliceWidth = ((confArray[i+1].timestampMs - confArray[i].timestampMs) / (confArray[confArray.length-1].timestampMs - confArray[0].timestampMs)) * C.innerWidth;
         ctx.fillStyle = C.colorMap[Math.round(confArray[i].confidence / C.colorMap.length)];
         ctx.fillRect(leftBound, C.top, sliceWidth, C.bottom - C.top);
     }
